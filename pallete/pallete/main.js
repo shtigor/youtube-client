c1158fac3c709ce4321ff4615331e5954bb6aa21
Igor Shturmak <shturmak.i.i@gmail.com>
2019-04-30 13:39:39 -0700

    refactor: begin apply eslint

--- pallete/pallete/main.js
+++ pallete/pallete/main.js
@@ -1,44 +1,40 @@
-var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d")
-var RectCount = 8
-var CircleCount = 1
-var figureRowCount = 3
-var figureColumnCount = 3
-var figureHeight = 150
-var figureWidth = 150
-var figurePadding = 10
-var figureOffsetTop = 0
-var figureOffsetLeft = 0
-var bucket_select = false
-var picker_select = false
-var transform_select = false
-var move_select = false
-var active_figure
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const figureRowCount = 3;
+const figureColumnCount = 3;
+const figureHeight = 150;
+const figureWidth = 150;
+const figurePadding = 10;
+const figureOffsetTop = 0;
+const figureOffsetLeft = 0;
+let bucketSelect = false;
+let pickerSelect = false;
+let transformSelect = false;
+let moveSelect = false;
+let activeFigure;
 
 // COLORS
-var current_color = ""
-var prev_color = ""
-var red_color = ""
-var blue_color = ""
+let currentColor = '';
+let prevColor = '';
+let redColor = '';
+let blueColor = '';
 
 
-
-
-var figures = [
+const figures = [
   [
-    {x: 0, y: 0, color: "#cccccc", shape: "rec", active: false}, 
-    {x: 0, y: 0, color: "#bababa", shape: "rec", active: false},
-    {x: 0, y: 0, color: "#c7c7c7", shape: "circle", active: false}
+    { x: 0, y: 0, color: '#cccccc', shape: 'rec', active: false }, 
+    { x: 0, y: 0, color: '#bababa', shape: 'rec', active: false },
+    { x: 0, y: 0, color: '#c7c7c7', shape: 'circle', active: false }
   ],
   [
-    {x: 0, y: 0, color: "#bdbdbd", shape: "rec", active: false},
-    {x: 0, y: 0, color: "#cfcfcf", shape: "rec", active: false},
-    {x: 0, y: 0, color: "#c4c4c4", shape: "rec", active: false}
+    { x: 0, y: 0, color: '#bdbdbd', shape: 'rec', active: false },
+    { x: 0, y: 0, color: '#cfcfcf', shape: 'rec', active: false },
+    { x: 0, y: 0, color: '#c4c4c4', shape: 'rec', active: false }
   ],
   [
-    {x: 0, y: 0, color: "#bfbfbf", shape: "rec", active: false},
-    {x: 0, y: 0, color: "#c9c9c9", shape: "rec", active: false},
-    {x: 0, y: 0, color: "#cccccc", shape: "rec", active: false}
+    { x: 0, y: 0, color: '#bfbfbf', shape: 'rec', active: false },
+    { x: 0, y: 0, color: '#c9c9c9', shape: 'rec', active: false },
+    { x: 0, y: 0, color: '#cccccc', shape: 'rec', active: false }
   ]
 ]
 // for (var c = 0; c < figureColumnCount; c++) {
@@ -49,56 +45,55 @@ var figures = [
 // }
 
 
+document.getElementsByClassName('colors__ul')[0].addEventListener('click', changeColor, false);
 
-document.getElementsByClassName("colors__ul")[0].addEventListener("click", changeColor, false)
-
-document.getElementsByClassName("pallete__ul--bucket")[0].addEventListener("click", selectBucket, false)
-document.body.addEventListener("keypress", (event) => {
-  if (event.code == "KeyB") {
-    bucket_select = true
-    picker_select = false
-    transform_select = false
-    move_select = false
+document.getElementsByClassName('pallete__ul--bucket')[0].addEventListener('click', selectBucket, false);
+document.body.addEventListener('keypress', (event) => {
+  if (event.code === 'KeyB') {
+    bucketSelect = true;
+    pickerSelect = false;
+    transformSelect = false;
+    moveSelect = false;
   }
-}, false)
-canvas.addEventListener("click", onClick, false);
+}, false);
+canvas.addEventListener('click', onClick, false);
 
-document.getElementsByClassName("pallete__ul--color")[0].addEventListener("click", selectColorPicker, false)
-document.body.addEventListener("keypress", (event) => {
-  if (event.code == "KeyC") {
-    bucket_select = false
-    picker_select = true
-    transform_select = false
-    move_select = false
+document.getElementsByClassName('pallete__ul--color')[0].addEventListener('click', selectColorPicker, false);
+document.body.addEventListener('keypress', (event) => {
+  if (event.code === 'KeyC') {
+    bucketSelect = false;
+    pickerSelect = true;
+    transformSelect = false;
+    moveSelect = false;
   }
-}, false)
-canvas.addEventListener("click", pickerColor, false)
+}, false);
+canvas.addEventListener('click', pickerColor, false);
 
-document.getElementsByClassName("pallete__ul--transform")[0].addEventListener("click", selectTransform, false)
-document.body.addEventListener("keypress", (event) => {
-  if (event.code == "KeyT") {
-    bucket_select = false
-    picker_select = false
-    transform_select = true
-    move_select = false
+document.getElementsByClassName('pallete__ul--transform')[0].addEventListener('click', selectTransform, false);
+document.body.addEventListener('keypress', (event) => {
+  if (event.code === 'KeyT') {
+    bucketSelect = false;
+    pickerSelect = false;
+    transformSelect = true;
+    moveSelect = false;
   }
-}, false)
-canvas.addEventListener("click", changeFigure, false)
+}, false);
+canvas.addEventListener('click', changeFigure, false);
 
-document.getElementsByClassName("pallete__ul--move")[0].addEventListener("click", selectMove, false)
-document.body.addEventListener("keypress", (event) => {
-  if (event.code == "KeyM") {
-    bucket_select = false
-    picker_select = false
-    transform_select = false
-    move_select = true
+document.getElementsByClassName('pallete__ul--move')[0].addEventListener('click', selectMove, false);
+document.body.addEventListener('keypress', (event) => {
+  if (event.code === 'KeyM') {
+    bucketSelect = false;
+    pickerSelect = false;
+    transformSelect = false;
+    moveSelect = true;
   }
-}, false)
-canvas.addEventListener("mousedown", moveFigure, false)
+}, false);
+canvas.addEventListener("mousedown", moveFigure, false);
 
 
 function drawBasic() {
-  for (var c = 0; c < figureColumnCount; c++) {
+  for (let c = 0; c < figureColumnCount; c++) {
     for (var r = 0; r < figureRowCount; r++) {
       var figureX = (c*(figureWidth+figurePadding))+figureOffsetLeft;
       var figureY = (r*(figureHeight+figurePadding))+figureOffsetTop;
@@ -134,50 +129,50 @@ function drawBasic() {
 
 function selectBucket(event) {
   if (event) {
-    bucket_select = true
-    picker_select = false
-    transform_select = false
-    move_select = false
+    bucketSelect = true
+    pickerSelect = false
+    transformSelect = false
+    moveSelect = false
   }
 }
 
 function selectTransform(event) {
   if (event) {
-    transform_select = true
-    bucket_select = false
-    picker_select = false
-    move_select = false
+    transformSelect = true
+    bucketSelect = false
+    pickerSelect = false
+    moveSelect = false
   }
 }
 
 function selectColorPicker(event) {
   if (event) {
-    picker_select = true
-    bucket_select = false
-    transform_select = false
-    move_select = false
+    pickerSelect = true
+    bucketSelect = false
+    transformSelect = false
+    moveSelect = false
   }
 }
 
 function selectMove(event) {
   if (event) {
-    move_select = true
-    picker_select = false
-    bucket_select = false
-    transform_select = false
+    moveSelect = true
+    pickerSelect = false
+    bucketSelect = false
+    transformSelect = false
   }
 }
 
 
 function onClick(event) {
-  if (bucket_select) {
+  if (bucketSelect) {
     var mx = event.layerX
     var my = event.layerY
     // console.log(mx + " " + my)
     for (var c = 0; c < figureColumnCount; c++) {
       for (var r = 0; r < figureRowCount; r++) {
         if ((mx >= figures[c][r].x && mx <= figures[c][r].x + figureWidth) && (my >= figures[c][r].y && my <= figures[c][r].y + figureHeight)) {
-          figures[c][r].color = current_color
+          figures[c][r].color = currentColor
         }
       }
     }
@@ -186,7 +181,7 @@ function onClick(event) {
 
 
 function changeFigure(event) {
-  if (transform_select) {
+  if (transformSelect) {
 
     var x = event.layerX
     var y = event.layerY
@@ -207,7 +202,7 @@ function changeFigure(event) {
 
 
 function pickerColor(event) {
-  if (picker_select) {
+  if (pickerSelect) {
   
   var x = event.layerX
   var y = event.layerY
@@ -224,7 +219,7 @@ function pickerColor(event) {
 
 
 function moveFigure(event) {
-  if (move_select) {
+  if (moveSelect) {
 
     var x = event.layerX
     var y = event.layerY
@@ -233,7 +228,7 @@ function moveFigure(event) {
       for (var r = 0; r < figureRowCount; r++) {
         if ((x >= figures[c][r].x && x <= figures[c][r].x + figureWidth) && (y >= figures[c][r].y && y <= figures[c][r].y + figureHeight)) {
           figures[c][r].active = true
-          active_figure = figures[c][r]
+          activeFigure = figures[c][r]
         }
       }
     }
@@ -242,9 +237,9 @@ function moveFigure(event) {
       var x = event.layerX
       var y = event.layerY
 
-      if ((x >= active_figure.x && x <= active_figure.x + figureWidth) && (y >= active_figure.y && y <= active_figure.y + figureHeight)) {
-        active_figure.x = event.layerX - figureWidth / 2
-        active_figure.y = event.layerY - figureHeight / 2
+      if ((x >= activeFigure.x && x <= activeFigure.x + figureWidth) && (y >= activeFigure.y && y <= activeFigure.y + figureHeight)) {
+        activeFigure.x = event.layerX - figureWidth / 2
+        activeFigure.y = event.layerY - figureHeight / 2
       }
     })
 
@@ -252,38 +247,38 @@ function moveFigure(event) {
 }
 
 canvas.addEventListener("mouseup", (event) => {
-  // active_figure.x = event.layerX - figureWidth / 2
-  // active_figure.y = event.layerY - figureHeight / 2
+  // activeFigure.x = event.layerX - figureWidth / 2
+  // activeFigure.y = event.layerY - figureHeight / 2
 
-  active_figure = ""
+  activeFigure = ""
 })
 
 
 
 function current_colors() {
-  current_color = getComputedStyle(document.getElementsByClassName("circle_current-color")[0]).backgroundColor
-  prev_color = getComputedStyle(document.getElementsByClassName("circle_prev-color")[0]).backgroundColor
-  red_color = getComputedStyle(document.getElementsByClassName("circle_red-color")[0]).backgroundColor
-  blue_color = getComputedStyle(document.getElementsByClassName("circle_blue-color")[0]).backgroundColor
+  currentColor = getComputedStyle(document.getElementsByClassName("circle_current-color")[0]).backgroundColor
+  prevColor = getComputedStyle(document.getElementsByClassName("circle_prev-color")[0]).backgroundColor
+  redColor = getComputedStyle(document.getElementsByClassName("circle_red-color")[0]).backgroundColor
+  blueColor = getComputedStyle(document.getElementsByClassName("circle_blue-color")[0]).backgroundColor
 }
 
 function changeColor() {
   // RED
   document.getElementsByClassName("colors__ul--red")[0].addEventListener("click", () => {
-    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = current_color
-    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = red_color 
+    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = currentColor
+    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = redColor 
   })
 
   // BLUE
   document.getElementsByClassName("colors__ul--blue")[0].addEventListener("click", () => {
-    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = current_color
-    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = blue_color 
+    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = currentColor
+    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = blueColor 
   })
 
   // PREVIOUS
   document.getElementsByClassName("colors__ul--prev-color")[0].addEventListener("click", () => {
-    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = current_color
-    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = prev_color 
+    document.getElementsByClassName("circle_prev-color")[0].style.backgroundColor = currentColor
+    document.getElementsByClassName("circle_current-color")[0].style.backgroundColor = prevColor 
   })
 }
 
